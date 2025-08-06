1c07a229d0217f97bb751c8c51cc158d01d22382
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-08-06 07:18:48 +0200

    feat: add initial frontend commit

---
+++ book-mgmt-basic-imperative-react/src/main/frontend/vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+
+	server: {
+		port: 3000,
+		proxy: {
+			'/genres': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+			'/publishers': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+			'/authors': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+			'/bookshops': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+			'/bookbookshops': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+			'/books': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			}
+		}
+	}
+})
