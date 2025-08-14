fa6511234d193d5332894107ce00257e2162b826
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-08-14 07:16:12 +0200

    feat: add pagination dtos

---
+++ book-mgmt-hex-imperative-react/src/main/java/dev/ime/application/dto/PaginatedResponse.java
@@ -0,0 +1,14 @@
+package dev.ime.application.dto;
+
+import java.util.List;
+
+import dev.ime.domain.model.PaginatedResult;
+
+public record PaginatedResponse<T>(
+	    List<T> content,
+	    int page,
+	    int size,
+	    long totalElements,
+	    int totalPages,
+	    boolean last) implements PaginatedResult<T>  {
+}
