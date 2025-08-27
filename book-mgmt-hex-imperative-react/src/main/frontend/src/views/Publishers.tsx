cddd985027396d4e91a90fc4f6f5403500696f61
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-08-27 07:16:42 +0200

    feat: add Publishers View

---
+++ book-mgmt-hex-imperative-react/src/main/frontend/src/views/Publishers.tsx
@@ -0,0 +1,11 @@
+import PublisherListContainer from '../containers/PublisherListContainer';
+
+const Publishers: React.FC = () => {
+  return (
+    <div className="container">
+      <PublisherListContainer />
+    </div>
+  );
+};
+
+export default Publishers;
