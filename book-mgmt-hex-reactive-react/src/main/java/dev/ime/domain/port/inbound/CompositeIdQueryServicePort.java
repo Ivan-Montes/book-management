4d051f5fd7c6c3f1505d68544dc576fd630ebdb1
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-03 09:01:27 +0200

    feat: add query service ports

---
+++ book-mgmt-hex-reactive-react/src/main/java/dev/ime/domain/port/inbound/CompositeIdQueryServicePort.java
@@ -0,0 +1,14 @@
+package dev.ime.domain.port.inbound;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import reactor.core.publisher.Mono;
+
+public interface CompositeIdQueryServicePort<T> {
+
+	Mono<Page<T>> getAll(Pageable pageable);
+	Mono<T> getById(UUID id01, UUID id02);
+}
