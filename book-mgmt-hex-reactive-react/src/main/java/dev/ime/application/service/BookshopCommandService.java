be42840d09282276aa121db94105d326455cb489
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-10 07:23:36 +0200

    feat: add Bookshop Service implementations

---
+++ book-mgmt-hex-reactive-react/src/main/java/dev/ime/application/service/BookshopCommandService.java
@@ -0,0 +1,81 @@
+package dev.ime.application.service;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import dev.ime.application.dto.BookshopDto;
+import dev.ime.application.exception.ResourceNotFoundException;
+import dev.ime.application.usecases.command.CreateBookshopCommand;
+import dev.ime.application.usecases.command.DeleteBookshopCommand;
+import dev.ime.application.usecases.command.UpdateBookshopCommand;
+import dev.ime.common.constants.GlobalConstants;
+import dev.ime.domain.command.Command;
+import dev.ime.domain.model.Event;
+import dev.ime.domain.port.inbound.CommandDispatcher;
+import dev.ime.domain.port.inbound.CommandServicePort;
+import dev.ime.domain.port.outbound.PublisherPort;
+import reactor.core.publisher.Mono;
+
+@Service
+public class BookshopCommandService implements CommandServicePort<BookshopDto> {
+
+	private final CommandDispatcher commandDispatcher;
+	private final PublisherPort publisherPort;
+	private static final Logger logger = LoggerFactory.getLogger(BookshopCommandService.class);
+	
+	public BookshopCommandService(@Qualifier("bookshopCommandDispatcher")CommandDispatcher commandDispatcher, PublisherPort publisherPort) {
+		super();
+		this.commandDispatcher = commandDispatcher;
+		this.publisherPort = publisherPort;
+	}
+
+	@Override
+	public Mono<Event> create(BookshopDto item) {
+
+		return Mono.justOrEmpty(item)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.BS_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(dto -> new CreateBookshopCommand(UUID.randomUUID(), dto.name()))
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	@Override
+	public Mono<Event> update(UUID id, BookshopDto item) {
+		
+		return Mono.justOrEmpty(item)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.BS_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(dto -> new UpdateBookshopCommand(id, dto.name()))
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	@Override
+	public Mono<Event> deleteById(UUID id) {
+
+		return Mono.justOrEmpty(id)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.BS_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(DeleteBookshopCommand::new)
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	private Mono<Event> runHandler(Command command){
+		
+		return Mono.just(command)
+				.map(commandDispatcher::getCommandHandler)
+				.flatMap( handler -> handler.handle(command));		
+	}
+	
+	private Mono<Event> processEvents(Event event) {
+		
+	    return Mono.just(event)	
+	        .doOnNext(eventItem -> logger.info(GlobalConstants.MSG_PATTERN_INFO, GlobalConstants.EVENT_CAT, eventItem))
+	    .flatMap(eventItem -> publisherPort.publishEvent(event).thenReturn(event))
+        .thenReturn(event);
+	}
+}
