e19bfb2e9b5b55df29d131456051daa24160066f
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-24 07:22:47 +0200

    feat: add Author data structures

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/domain/model/Author.java
@@ -0,0 +1,94 @@
+package dev.ime.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+public class Author {
+
+	private UUID authorId;
+	private String name;
+	private String surname;
+	private LocalDateTime creationTimestamp;
+	private LocalDateTime updateTimestamp;
+	private Set<Book>books = new HashSet<>();
+	
+	public Author() {
+		super();
+	}
+	
+	public Author(UUID authorId, String name, String surname, LocalDateTime creationTimestamp,
+			LocalDateTime updateTimestamp, Set<Book> books) {
+		super();
+		this.authorId = authorId;
+		this.name = name;
+		this.surname = surname;
+		this.creationTimestamp = creationTimestamp;
+		this.updateTimestamp = updateTimestamp;
+		this.books = books;
+	}
+	
+	public UUID getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(UUID authorId) {
+		this.authorId = authorId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public LocalDateTime getCreationTimestamp() {
+		return creationTimestamp;
+	}
+	public void setCreationTimestamp(LocalDateTime creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+	public LocalDateTime getUpdateTimestamp() {
+		return updateTimestamp;
+	}
+	public void setUpdateTimestamp(LocalDateTime updateTimestamp) {
+		this.updateTimestamp = updateTimestamp;
+	}
+	public Set<Book> getBooks() {
+		return books;
+	}	
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(authorId, creationTimestamp, name, surname, updateTimestamp);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		return Objects.equals(authorId, other.authorId) && Objects.equals(creationTimestamp, other.creationTimestamp)
+				&& Objects.equals(name, other.name) && Objects.equals(surname, other.surname)
+				&& Objects.equals(updateTimestamp, other.updateTimestamp);
+	}
+	
+	@Override
+	public String toString() {
+		return "Author [authorId=" + authorId + ", name=" + name + ", surname=" + surname + ", creationTimestamp="
+				+ creationTimestamp + ", updateTimestamp=" + updateTimestamp + "]";
+	}
+}
