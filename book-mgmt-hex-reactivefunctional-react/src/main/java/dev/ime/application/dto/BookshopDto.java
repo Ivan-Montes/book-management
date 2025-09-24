14eb9fbb81b35e5f1a0921f590473e2f6268a930
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-24 07:25:31 +0200

    feat: add Bookshop data structures

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/application/dto/BookshopDto.java
@@ -0,0 +1,22 @@
+package dev.ime.application.dto;
+
+import java.util.UUID;
+
+import dev.ime.common.constants.GlobalConstants;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record BookshopDto(
+		UUID bookshopId,
+		@NotBlank
+		@Size( min = 1, max = 50)
+		@Pattern( regexp = GlobalConstants.PATTERN_NAME_FULL)
+		String name
+		) {
+	public BookshopDto {
+        if (name != null) {
+            name = name.trim();
+        }
+    }
+}
---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/domain/model/Bookshop.java
@@ -0,0 +1,94 @@
+package dev.ime.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+public class Bookshop {
+	private UUID bookshopId;
+	private String name;
+	private Set<BookBookshop> books = new HashSet<>();
+	private LocalDateTime creationTimestamp;
+	private LocalDateTime updateTimestamp;
+	
+	public Bookshop() {
+		super();
+	}
+
+	public Bookshop(UUID bookshopId, String name, Set<BookBookshop> books, LocalDateTime creationTimestamp,
+			LocalDateTime updateTimestamp) {
+		super();
+		this.bookshopId = bookshopId;
+		this.name = name;
+		this.books = books;
+		this.creationTimestamp = creationTimestamp;
+		this.updateTimestamp = updateTimestamp;
+	}
+
+	public UUID getBookshopId() {
+		return bookshopId;
+	}
+
+	public void setBookshopId(UUID bookshopId) {
+		this.bookshopId = bookshopId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<BookBookshop> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<BookBookshop> books) {
+		this.books = books;
+	}
+
+	public LocalDateTime getCreationTimestamp() {
+		return creationTimestamp;
+	}
+
+	public void setCreationTimestamp(LocalDateTime creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+
+	public LocalDateTime getUpdateTimestamp() {
+		return updateTimestamp;
+	}
+
+	public void setUpdateTimestamp(LocalDateTime updateTimestamp) {
+		this.updateTimestamp = updateTimestamp;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bookshopId, creationTimestamp, name, updateTimestamp);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bookshop other = (Bookshop) obj;
+		return Objects.equals(bookshopId, other.bookshopId)
+				&& Objects.equals(creationTimestamp, other.creationTimestamp) && Objects.equals(name, other.name)
+				&& Objects.equals(updateTimestamp, other.updateTimestamp);
+	}
+
+	@Override
+	public String toString() {
+		return "Bookshop [bookshopId=" + bookshopId + ", name=" + name + ", creationTimestamp=" + creationTimestamp
+				+ ", updateTimestamp=" + updateTimestamp + "]";
+	}		
+}
