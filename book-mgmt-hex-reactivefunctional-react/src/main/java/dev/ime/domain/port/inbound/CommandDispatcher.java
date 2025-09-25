1fd088de751b291b29d59a17cbd7840389df8cec
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-25 07:31:50 +0200

    feat: add dispatcher interfaces

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/domain/port/inbound/CommandDispatcher.java
@@ -0,0 +1,9 @@
+package dev.ime.domain.port.inbound;
+
+import dev.ime.domain.command.Command;
+import dev.ime.domain.command.CommandHandler;
+
+public interface CommandDispatcher {
+
+	CommandHandler getCommandHandler(Command command);	
+}
