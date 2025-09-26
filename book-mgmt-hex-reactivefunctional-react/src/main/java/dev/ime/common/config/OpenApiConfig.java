e84977ee1381decfccb79a1817155e49c97dc295
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-09-26 07:28:28 +0200

    feat: add Security config

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/common/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package dev.ime.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("book-management")
+                        .version("1.0")
+                        .description("book-mgmt-hex-reactivefunctional-react"));
+    }
+}
