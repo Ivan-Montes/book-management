e09fdbe08be2865a4d729550cd50c5953ddec5dc
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-10-07 07:23:06 +0200

    feat: add Publisher adapters

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/java/dev/ime/infrastructure/adapter/PublisherProjectorAdapter.java
@@ -0,0 +1,229 @@
+package dev.ime.infrastructure.adapter;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.hibernate.reactive.mutiny.Mutiny.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import dev.ime.application.exception.CreateJpaEntityException;
+import dev.ime.application.exception.EmptyResponseException;
+import dev.ime.application.exception.EntityAssociatedException;
+import dev.ime.application.exception.ResourceNotFoundException;
+import dev.ime.application.exception.UniqueValueException;
+import dev.ime.application.utils.LogHelper;
+import dev.ime.application.utils.MapExtractorHelper;
+import dev.ime.application.utils.PrintHelper;
+import dev.ime.common.constants.GlobalConstants;
+import dev.ime.domain.model.Event;
+import dev.ime.domain.port.outbound.ProjectorPort;
+import dev.ime.infrastructure.entity.PublisherJpaEntity;
+import io.smallrye.mutiny.converters.uni.UniReactorConverters;
+import reactor.core.publisher.Mono;
+
+@Repository
+@Qualifier("publisherProjectorAdapter")
+public class PublisherProjectorAdapter implements ProjectorPort {
+
+	private final SessionFactory sessionFactory;
+	private final MapExtractorHelper mapExtractorHelper;
+	private final PrintHelper printHelper;
+	private final LogHelper logHelper;
+	private static final Logger logger = LoggerFactory.getLogger(PublisherProjectorAdapter.class);
+
+	public PublisherProjectorAdapter(SessionFactory sessionFactory, MapExtractorHelper mapExtractorHelper,
+			PrintHelper printHelper, LogHelper logHelper) {
+		super();
+		this.sessionFactory = sessionFactory;
+		this.mapExtractorHelper = mapExtractorHelper;
+		this.printHelper = printHelper;
+		this.logHelper = logHelper;
+	}
+
+	@Override
+	public Mono<Void> create(Event event) {
+
+		return Mono.justOrEmpty(event.getEventData())	
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.flatMap(this::createJpaEntity)
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.flatMap(this::validateCreate)
+				.flatMap(this::insertAction)
+				.switchIfEmpty(Mono.error(new EmptyResponseException(Map.of(GlobalConstants.PUBLI_CAT, GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform( flow -> logHelper.addFinalLog(flow, printHelper, logger))
+				.then();
+	}
+
+	private Mono<PublisherJpaEntity> createJpaEntity(Map<String, Object> eventData) {
+		
+		return Mono.fromCallable( () -> {
+			
+			UUID publisherId = mapExtractorHelper.extractUuid(eventData, GlobalConstants.PUBLI_ID);
+			String name = mapExtractorHelper.extractAsString(eventData, GlobalConstants.MODEL_NAME,
+					GlobalConstants.PATTERN_NAME_FULL);
+
+			return PublisherJpaEntity.builder().publisherId(publisherId).name(name).build();
+			
+		}).onErrorMap(e -> new CreateJpaEntityException(Map.of(GlobalConstants.PUBLI_CAT, e.getMessage())));		
+	}
+
+	private Mono<PublisherJpaEntity> validateCreate(PublisherJpaEntity entity) {
+		
+		return Mono.just(entity)
+				.flatMap(this::checkNameAvailability)
+				.thenReturn(entity);
+	}
+
+	private Mono<PublisherJpaEntity> checkNameAvailability(PublisherJpaEntity entity) {
+
+		String query = """
+				SELECT
+				 COUNT(e) 
+				FROM
+				 PublisherJpaEntity e 
+				WHERE
+				 e.name = :name
+				""";
+		
+		return sessionFactory
+				.withSession(
+						session -> session.createQuery(query, Long.class).setParameter(GlobalConstants.MODEL_NAME, entity.getName())
+								.getSingleResult().map(count -> count == 0))
+				.convert().with(UniReactorConverters.toMono()).filter(b -> b)
+				.switchIfEmpty(Mono.error(new UniqueValueException(
+						Map.of(GlobalConstants.PUBLI_NAME, entity.getName()))))
+				.thenReturn(entity);
+	}
+	
+	private Mono<PublisherJpaEntity> insertAction(PublisherJpaEntity entity) {
+
+		return sessionFactory
+		.withTransaction((session, tx) -> session.persist(entity)
+				.replaceWith(entity))
+		.convert().with(UniReactorConverters.toMono());
+	}
+
+	@Override
+	public Mono<Void> update(Event event) {
+
+		return  Mono.justOrEmpty(event.getEventData())	
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.flatMap(this::createJpaEntity)
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.flatMap(this::validateUpdate)	
+				.flatMap(this::updateAction)	
+				.switchIfEmpty(Mono.error(new EmptyResponseException(Map.of(GlobalConstants.PUBLI_CAT, GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform( flow -> logHelper.addFinalLog(flow, printHelper, logger))
+				.then();
+	}
+
+	private Mono<PublisherJpaEntity> validateUpdate(PublisherJpaEntity entity) {
+		
+		return Mono.just(entity)
+				.flatMap(this::checkNameAvailabilityInUpdateAction)
+				.thenReturn(entity);
+	}
+	
+	private Mono<PublisherJpaEntity> checkNameAvailabilityInUpdateAction(PublisherJpaEntity entity) {
+
+		String query = """
+				SELECT
+				 COUNT(e)
+				FROM
+				 PublisherJpaEntity e
+				WHERE
+				 e.name = :name AND
+				 e.publisherId != :publisherId
+				""";
+
+		return sessionFactory
+				.withSession(session -> session.createQuery(query, Long.class)
+						.setParameter(GlobalConstants.MODEL_NAME, entity.getName())
+						.setParameter(GlobalConstants.PUBLI_ID, entity.getPublisherId()).getSingleResult()
+						.map(count -> count == 0))
+				.convert().with(UniReactorConverters.toMono()).filter(b -> b)
+				.switchIfEmpty(Mono.error(new UniqueValueException(
+						Map.of(GlobalConstants.PUBLI_NAME, entity.getName()))))
+				.thenReturn(entity);
+	}
+	
+	private Mono<PublisherJpaEntity> updateAction(PublisherJpaEntity entity) {
+	
+		String query = """
+				SELECT
+				 e 
+				FROM
+				 PublisherJpaEntity e 
+				WHERE
+				 e.publisherId = :publisherId
+				""";
+		
+		return sessionFactory
+		.withTransaction((session, tx) -> session.createQuery(query, PublisherJpaEntity.class)
+				.setParameter(GlobalConstants.PUBLI_ID, entity.getPublisherId())
+				.getSingleResult()
+	            .flatMap(item -> {
+	            	item.setName(entity.getName());
+	                return session.merge(item);
+	            }))
+				.convert().with(UniReactorConverters.toMono());	
+	}
+
+	@Override
+	public Mono<Void> deleteById(Event event) {
+
+		return Mono.justOrEmpty(event.getEventData())
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.map(evenData -> evenData.get(GlobalConstants.PUBLI_ID))
+				.cast(String.class)
+				.map(UUID::fromString)
+				.doOnNext(data -> logHelper.logFlowStep(data, printHelper, logger))
+				.flatMap(this::validateDelete)	
+				.flatMap(this::deleteByIdAction)
+				.switchIfEmpty(Mono.error(new EmptyResponseException(
+						Map.of(GlobalConstants.PUBLI_CAT, GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform( flow -> logHelper.addFinalLog(flow, printHelper, logger))
+				.then();
+	}
+
+	private Mono<UUID> validateDelete(UUID entityId) {
+		
+		return Mono.just(entityId)
+				.flatMap(this::checkEntityAssociation)
+				.thenReturn(entityId);
+	}
+	
+	private Mono<UUID> checkEntityAssociation(UUID entityId){
+		
+		String query = """
+				SELECT
+				 COUNT(*)
+				FROM
+				 BOOKS e 
+				WHERE
+				 e.publisher_id = ?1
+				""";		
+		
+		return sessionFactory
+				.withSession(session -> session.createNativeQuery(query, Long.class)
+						.setParameter(1, entityId)
+						.getSingleResult())
+						.convert().with(UniReactorConverters.toMono())
+						.filter( i -> i == 0)
+						.switchIfEmpty(Mono.error(new EntityAssociatedException(Map.of(GlobalConstants.PUBLI_ID, entityId.toString()))))
+						.thenReturn(entityId);
+	}
+	
+	private Mono<PublisherJpaEntity> deleteByIdAction(UUID entityId) {
+		
+	    return sessionFactory.withTransaction((session, tx) -> session.find(PublisherJpaEntity.class, entityId)
+	            .onItem().ifNull().failWith(() -> new ResourceNotFoundException(
+	                Map.of(GlobalConstants.PUBLI_ID, entityId.toString())))
+	            .call(session::remove))
+	            .convert()
+	    		.with(UniReactorConverters.toMono());	
+	}
+}
