4530a8eb9f0a489ab30280a909bbeb3784c87b65
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-10-10 07:25:21 +0200

    feat: add data views

---
+++ book-mgmt-hex-reactivefunctional-react/src/main/frontend/src/views/Books.tsx
@@ -0,0 +1,11 @@
+import BookListContainer from '../containers/BookListContainer';
+
+const Books: React.FC = () => {
+  return (
+    <div className="container">
+      <BookListContainer />
+    </div>
+  );
+};
+
+export default Books;
